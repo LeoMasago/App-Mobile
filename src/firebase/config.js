b9f8fbf87b8289b7f3c4e423a645151efe2bc9c8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-14 20:17:22 -0300

    commit aula 4

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,6 +4,7 @@ import { getDatabase } from 'firebase/database';
 import {
   API_KEY,
   AUTH_DOMAIN,
+  DATABASE_URL,
   PROJECT_ID,
   STORAGE_BUCKET,
   MESSAGING_SENDER_ID,
@@ -14,6 +15,7 @@ import {
 const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
--- src/firebase/productService.js
+++ src/firebase/productService.js
@@ -4,7 +4,7 @@ import { db } from './config';
 export async function createProduct(product) {
   const productsRef = ref(db, 'products');
   const newProductRef = push(productsRef);
-
+  
   await set(newProductRef, product);
 
   return newProductRef.key;
